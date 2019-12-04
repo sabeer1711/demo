c5757c3604e8d515dc8fe225706572ae805e594f
Sabeer Ahamed <sabeer1711>
2019-12-04 22:01:43 +0530

    created a ngClass property

--- demo/src/app/server/server.component.ts
+++ demo/src/app/server/server.component.ts
@@ -2,20 +2,29 @@ import {Component} from '@angular/core';
 
 @Component({
     selector:'app-server',
-    templateUrl:'./server.component.html'
-
+    templateUrl:'./server.component.html',
+    styles:[`
+    .online{
+            color:white;
+        }      
+         `   ]
 })
 export class ServerComponent{
 
-
+server="first";
 serverId=10;
 serverStatus:string = 'offline';
+constructor(){
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline' ;
+}
     
 status(){
     return this.serverStatus
     
 }
-
+getColor(){
+   return this.serverStatus === 'online' ? 'green' : 'red';
+}
 
 
 
