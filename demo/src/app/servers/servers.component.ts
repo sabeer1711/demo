8fb55cdaa05062f755c1f60104ff3d4e24b4a243
Sabeer Ahamed <sabeer1711>
2019-12-04 23:26:48 +0530

    ngFor using the properties

--- demo/src/app/servers/servers.component.ts
+++ demo/src/app/servers/servers.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   servername ="testserver"
+  servers=["server1","server2"];
   serverCreated = false
   constructor() { 
    setTimeout(() =>{
@@ -25,11 +26,18 @@ export class ServersComponent implements OnInit {
   
   createserverStatus(){
     this.serverCreated=true
+    this.servers.push(this.servername)
     return this.serverStatus="now will created the name is " + this.servername
   }
   Updateservername="first server"
   updatServername(event:Event){
     this.Updateservername =(<HTMLInputElement>event.target).value;
+  }
+  showsecret=false;
+  log=[];
+  onToggle(){
+    this.showsecret = !this.showsecret
+    this.log.push(this.log.length + 1);
   }
 
 }
