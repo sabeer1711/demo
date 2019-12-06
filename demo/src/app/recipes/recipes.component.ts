3c4e76dd8b3d5d3c6846c03c3421759ee7aab132
Wahith <wahith@hasnatech.com>
2019-12-06 19:18:48 +0530

    create a model

--- demo/src/app/recipes/recipes.component.ts
+++ demo/src/app/recipes/recipes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipes.model';
 
 @Component({
   selector: 'app-recipes',
@@ -7,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipesComponent implements OnInit {
 
+  recipes:Recipe[] = [
+    new Recipe('A Test recipe','This is simply test','https://www.seriouseats.com/2019/07/20190618-grilled-turkish-chicken-wings-vicky-wasik-13-1500x1125.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
