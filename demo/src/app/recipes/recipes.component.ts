679a3211d0b68d7b8fa60b01c265383c4eddc86d
Wahith <wahith@hasnatech.com>
2019-12-09 11:38:09 +0530

    now add a multiple recipes

--- demo/src/app/recipes/recipes.component.ts
+++ demo/src/app/recipes/recipes.component.ts
@@ -9,8 +9,10 @@ import { Recipe } from './recipes.model';
 export class RecipesComponent implements OnInit {
 
   recipes:Recipe[] = [
+    new Recipe('A Test recipe','This is simply test','https://www.seriouseats.com/2019/07/20190618-grilled-turkish-chicken-wings-vicky-wasik-13-1500x1125.jpg'),
     new Recipe('A Test recipe','This is simply test','https://www.seriouseats.com/2019/07/20190618-grilled-turkish-chicken-wings-vicky-wasik-13-1500x1125.jpg')
   ];
+  
   constructor() { }
 
   ngOnInit() {
