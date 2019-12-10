8142a6501bfc9a5c118aea3ea924a4ed7ff66a00
Sabeer Ahamed <sabeer1711>
2019-12-10 22:16:11 +0530

    Now its ok header

---
+++ demo/src/app/header/header.component.ts
@@ -0,0 +1,23 @@
+<<<<<<< HEAD
+import { Component } from '@angular/core';
+import { Template } from '@angular/compiler/src/render3/r3_ast';
+
+@Component({
+    selector:'app-header',
+    templateUrl:'./header.component.html'
+})
+
+=======
+
+import { Component } from '@angular/core';
+
+@Component({
+templateUrl:'./header.component.html',
+selector:'app-header',
+})
+
+
+>>>>>>> e7d0c2955f73980bda10adc7ee892b854f66b26f
+export class HeaderComponent{
+
+}
