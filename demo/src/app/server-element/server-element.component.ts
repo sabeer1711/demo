feb75e7c902be9999ed164e5bdfd6ec5dbe7ef5c
Wahith <wahith@hasnatech.com>
2019-12-12 20:09:31 +0530

    Hooks

--- demo/src/app/server-element/server-element.component.ts
+++ demo/src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input,ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input,ViewEncapsulation,OnChanges, SimpleChanges,DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,11 +6,22 @@ import { Component, OnInit, Input,ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation:ViewEncapsulation.None
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit,OnChanges,DoCheck{
  @Input('srvElement') element:{type:string,name:string ,content:string}
-  constructor() { }
+ @Input() name:string
+  constructor() { 
+    console.log('constructor worked')
+  }
 
   ngOnInit() {
+    console.log('ngOninit worked')
+  }
+  ngOnChanges(changes: SimpleChanges){
+    console.log("ng on change wokrd")
+    console.log(changes)
+  }
+  ngDoCheck(){
+
   }
 
 }
